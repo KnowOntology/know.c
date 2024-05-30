2f44e9d0803d62d090d1386164b1757cf895f513
Arto Bendiken <arto@haltia.ai>
2024-05-30 23:25:17 +0300

    Create stubs for all classes.

---
+++ src/know/classes/hindu_temple.h
@@ -0,0 +1,6 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#pragma once
+
+typedef struct {
+} hindu_temple;
---
+++ src/know/classes/synagogue.h
@@ -0,0 +1,6 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#pragma once
+
+typedef struct {
+} synagogue;
